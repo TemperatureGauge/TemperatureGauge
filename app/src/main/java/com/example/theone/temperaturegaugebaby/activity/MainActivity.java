38a8edb5abda432d5551d04a7e8af07763a2e8eb
nevergoneok <nevergoneok@163.com>
2016-04-08 21:34:36 +0800

    更新项目目录

--- app/src/main/java/com/example/theone/temperaturegaugebaby/activity/MainActivity.java
+++ app/src/main/java/com/example/theone/temperaturegaugebaby/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.theone.temperaturegaugebaby;
+package com.example.theone.temperaturegaugebaby.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.theone.temperaturegaugebaby.R;
 
 public class MainActivity extends AppCompatActivity {
 
